36def30504cafda6f95abdfa06bc03fb37b85be2
Gravitasta <thatmelissagraves@gmail.com>
2025-08-15 17:07:33 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,17 @@
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open('dogwalker-store').then(cache => cache.addAll(['./','./index.html','./manifest.json'])));
+  e.waitUntil(
+    caches.open('dogwalker-store').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json'
+      ]);
+    })
+  );
 });
+
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
 });
